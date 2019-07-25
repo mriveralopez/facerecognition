e2ee714f1b5e358e176abe040d39d09fe116fe79
mriveralopez <mrivera.lopez@outlook.com>
2019-07-24 21:39:08 -0600

    step 1 (100%)

--- step1.py
+++ step1.py
@@ -1,7 +1,7 @@
 import cv2
 import face_recognition
-import numpy as np
 import os
+import time
 
 path = './venv/lib/python3.7/site-packages/cv2/'
 
@@ -10,6 +10,10 @@ class Recognition:
         face_cascade = cv2.CascadeClassifier(path + 'data/haarcascade_frontalface_alt.xml')
 
         eye_cascade = cv2.CascadeClassifier(path + 'data/haarcascade_eye.xml')
+
+        know_faces = []
+        name_faces = []
+        r = 1
 
         cap = cv2.VideoCapture(0)
 
@@ -32,8 +36,11 @@ class Recognition:
             # if the 'q' key is pressed, stop the loop
             if key == ord("q"):
                 break
-            # if the spacebar key is pressed, save face
-            if key == ord(" "):
+
+            # if the 'r' key is pressed, reload know faces
+            if (key == ord("r") or r):
+                r = 0
+
                 directory = "./people/"
 
                 know_faces = []
@@ -48,48 +55,27 @@ class Recognition:
                     know_faces.append(image_encoding)
                     name_faces.append(name)
 
+            # if the spacebar key is pressed, save face
+            if key == ord(" "):
                 rgb_img = img[:, :, ::-1]
 
                 # Find all the faces and face encodings in the current frame of video
                 face_locations = face_recognition.face_locations(rgb_img)
                 face_encodings = face_recognition.face_encodings(rgb_img, face_locations)
 
+                match = []
+
                 for face_encoding in face_encodings:
                     match = face_recognition.compare_faces(know_faces, face_encoding, tolerance=0.50)
-                    print(match)
+
+                # print(match)
+                i = 0
+                while i < match.__len__():
+                    if(match[i]):
+                        file_name = name_faces[i] + " " + time.strftime("%d-%m-%y %H-%M-%S")
+                        print(file_name)
+                        cv2.imwrite('./rec/' + file_name + '.jpg', img)
+                    i = i + 1
 
         cap.release()
         cv2.destroyAllWindows()
-
-    def readFacesFromPicturesFiles(self, img):
-        import os
-        import face_recognition
-
-        directory = "./people/"
-
-        know_faces = []
-        name_faces = []
-
-        for file in os.listdir(directory):
-            # print(os.path.join(directory, file))
-            name = file.split(".")[0]
-            # print(name)
-            image = face_recognition.load_image_file(file)
-            image_encoding = face_recognition.face_encodings(image)[0]
-            know_faces.append(image_encoding)
-            name_faces.append(name)
-
-        Recognition.writeNames(img, know_faces, name_faces)
-
-    def writeNames(self, img, know_faces, names_faces):
-        import face_recognition
-        
-        rgb_img = img[:, :, ::-1]
-
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_img)
-        face_encodings = face_recognition.face_encodings(rgb_img, face_locations)
-
-        for face_encoding in face_encodings:
-            match = face_recognition.compare_faces(know_faces, face_encoding, tolerance=0.50)
-            print(match)
